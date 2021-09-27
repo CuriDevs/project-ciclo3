29cda4e900ce4fe7e080971438327640666bd6f7
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-09-26 19:24:10 -0500

    Escritura de Header

---
+++ src/Shared/components/Header.js
@@ -0,0 +1,22 @@
+import Navbar from 'react-bootstrap/Navbar'
+
+function Header() {
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src="/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />{' '}
+                React Bootstrap
+                </Navbar.Brand>
+            </Container>
+    </Navbar>
+    );
+}
+
+export default Header;
