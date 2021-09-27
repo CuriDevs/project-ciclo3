9237b9a992eaf1e0284e4862325bdcde69b5b282
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-09-26 22:06:10 -0500

    Creacion page Sales

--- src/Shared/components/Header.js
+++ src/Shared/components/Header.js
@@ -9,19 +9,27 @@ function Header() {
             <Container>
                 <Navbar.Brand href="#home">JALMED</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                </Navbar.Collapse>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Modulos" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Administracion de Ventas</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Administracion de Productos</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Administracion de usuarios</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Cerrar Sesion</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                        <img
+                        alt=""
+                        src="../Images/iconPngSinTexto.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        />{' '}
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+                    </Navbar.Collapse>
             </Container>
         </Navbar>
     );
