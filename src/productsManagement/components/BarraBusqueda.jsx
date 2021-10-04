a47ba41c1a779f66a6485b0fa20ed3e547b84c9e
JuanPabloRP <jpabloroldanp@gmail.com>
2021-10-04 16:31:25 -0500

    Estilo barra busqueda hecho

--- src/productsManagement/components/BarraBusqueda.jsx
+++ src/productsManagement/components/BarraBusqueda.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import searchImg from "../Images/search.png" 
 
 const BarraBusqueda = () => {
     return (
-        <form className="form-inline my-2 my-lg-0 containerBarraBusqueda">
-            <input className="form-control mr-sm-2 mx-1" type="search" placeholder="Buscar por nombre" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0 mx-1" type="submit">Buscar</button>
+        <form className="containerBarraBusqueda">
+            <input type="text" className="inputBarraBusqueda childrenBarraBusqueda" />
+            <button type="submit" className="btnBarraBusqueda childrenBarraBusqueda"><img src={searchImg} alt="search" /></button>
         </form>
     )
 }
 
 export default BarraBusqueda
+
+
+{/* <input className="form-control mr-sm-2 mx-1" type="search" placeholder="Buscar" aria-label="Search" />
+<button className="btn btn-outline-success my-2 my-sm-0 mx-1" type="submit">Buscar</button> */}
+        
+
+{/*  <input type="text" />
+            <select name="" id="">
+                <option value={null} ></option>
+                <option value="idProducto">Buscar por ID</option>
+                <option value="nombreProducto">Buscar por nombre</option>
+                <option value="descripcionProducto">Buscar por descripcion</option>
+            </select>
+            <input type="submit" /> */}
