75d02565f091cc6df639be27afd4f9b40b96b569
JuanPabloRP <jpabloroldanp@gmail.com>
2021-10-04 19:39:42 -0500

    cambio de estilos y cambio en la barra busqueda

--- src/productsManagement/components/BarraBusqueda.jsx
+++ src/productsManagement/components/BarraBusqueda.jsx
@@ -13,15 +13,3 @@ const BarraBusqueda = () => {
 export default BarraBusqueda
 
 
-{/* <input className="form-control mr-sm-2 mx-1" type="search" placeholder="Buscar" aria-label="Search" />
-<button className="btn btn-outline-success my-2 my-sm-0 mx-1" type="submit">Buscar</button> */}
-        
-
-{/*  <input type="text" />
-            <select name="" id="">
-                <option value={null} ></option>
-                <option value="idProducto">Buscar por ID</option>
-                <option value="nombreProducto">Buscar por nombre</option>
-                <option value="descripcionProducto">Buscar por descripcion</option>
-            </select>
-            <input type="submit" /> */}
