2d65cafa042d64fb10e79c8cd8f71f98315678ac
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-10-22 18:59:15 -0500

    Validacion de usuario y redireccion

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,12 +3,22 @@ import React, { useState, useEffect } from "react";
 import GoogleLogin from 'react-google-login';
 import logo from "../Images/Jalmedfactory.png";
 import nombre from "../Images/nombre.png";
-import { api } from '../Roles/utils/api';
+import { api } from '../utils/api';
+import { useHistory } from "react-router-dom";
 
 function Login() {
 
-  //const user = []
-  const value = new Date().toISOString();
+  const history = useHistory();
+
+  function navigateToHome() {
+    history.push("/sales");
+  }
+
+  function navigateToProducts() {
+    history.push("/products");
+  }
+
+  const value = new Date().toString();
 
   const [ user, setList ] = useState({
     UserName: "",
@@ -26,7 +36,37 @@ function Login() {
     }
   };
 
-  const login = (res) => {
+  const busqueda = async () => {
+    const resultado = await api.Users.list();
+    return resultado;
+  }
+
+  const validacion = (data, usuarios) => {
+    var create = "vacio";
+    for (let i=0; i<usuarios.length; i++) {
+      if (usuarios[i].Email == data.Email && usuarios[i].State == "Activo") {
+        console.log(usuarios[i].Email);
+        create = "Creado";
+      } else if (usuarios[i].Email == data.Email && usuarios[i].State == "Inactivo") {
+        create = "Inactivo";
+      } else {
+        create = "Por crear"
+      }
+    }
+
+    if (create == "Creado") {
+      navigateToProducts();
+      console.log('Redigido');
+    } else if (create == "Inactivo") {
+      console.log('Pendiente activacion')
+    } else {
+      creacion(data);
+      navigateToHome();
+      console.log('Usuario creado');
+    }
+  }
+
+  const login = async (res) => {
     localStorage.setItem("token", res.tokenId);
     setList(user.UserName = res.profileObj.name,
       user.Email = res.profileObj.email,
@@ -34,8 +74,9 @@ function Login() {
       user.Rol = "Cliente",
       user.State = "Inactivo");
     console.log(res);
-    console.log(user);
-    creacion(user);
+    const usuarios = await busqueda();
+    console.log(usuarios);
+    validacion(user, usuarios);
   };
 
   const loginError = (err) => {
@@ -55,7 +96,7 @@ function Login() {
           onSuccess={ login }
           onFailure={ loginError }
           cookiePolicy={ 'single_host_origin' }
-        />,
+        />
       </header>
     </div>
   );
